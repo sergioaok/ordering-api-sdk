5485620f810e2a0500dc5304231f6725b217e04a
Luis Mercado <lemf@MacBook-Pro-de-Luis.local>
2020-08-19 08:50:58 -0500

    Export new APIs

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,20 @@ import { Language, LanguageProps } from './models/Language'
 import { ApiLanguage } from './classes/ApiLanguage'
 import { Translation, TranslationProps } from './models/Translation'
 import { ApiTranslation } from './classes/ApiTranslation'
+import { Country, CountryProps } from './models/Country'
+import { ApiCountry } from './classes/ApiCountry'
+import { City, CityProps } from './models/City'
+import { ApiCity } from './classes/ApiCity'
+import { Page, PageProps } from './models/Page'
+import { ApiPage } from './classes/ApiPage'
+import { Category, CategoryProps } from './models/Category'
+import { ApiCategory } from './classes/ApiCategory'
+import { Product, ProductProps } from './models/Product'
+import { ApiProduct } from './classes/ApiProduct'
+import { Menu, MenuProps } from './models/Menu'
+import { ApiMenu } from './classes/ApiMenu'
+import { Config, ConfigProps } from './models/Config'
+import { ApiConfig } from './classes/ApiConfig'
 
 export {
   Ordering,
@@ -28,6 +42,13 @@ export {
   ApiValidationField,
   ApiLanguage,
   ApiTranslation,
+  ApiCountry,
+  ApiCity,
+  ApiPage,
+  ApiCategory,
+  ApiProduct,
+  ApiMenu,
+  ApiConfig,
   // Models
   Model,
   User,
@@ -43,5 +64,19 @@ export {
   Language,
   LanguageProps,
   Translation,
-  TranslationProps
+  TranslationProps,
+  Country,
+  CountryProps,
+  City,
+  CityProps,
+  Page,
+  PageProps,
+  Category,
+  CategoryProps,
+  Product,
+  ProductProps,
+  Menu,
+  MenuProps,
+  Config,
+  ConfigProps
 }
